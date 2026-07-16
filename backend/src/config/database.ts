7ef2e4f366e66e373d367ad6704660124bc6ad32
vermashobhit1994 <vermashobhit1994@gmail.com>
2026-07-16 19:27:27 +0530

    chore: auth backend implementation: scaffhold backend - test and establish connection to postgresql database

---
+++ backend/src/config/database.ts
@@ -0,0 +1,38 @@
+import { Pool, Client } from "pg";
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const port = Number(process.env.DB_PORT);
+if (Number.isNaN(port)) {
+    throw new Error(`DATABASE PORT is invalid`);
+}
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+    maxLifetimeSeconds: 60,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+});
+
+
+export async function checkDBConnection() {
+    pool.query("SELECT 1 AS STATUS", (err, resp) => {
+        if (err) {
+            throw new Error(err.message)
+        }
+        else if (resp.rows[0].status === 1) {
+            console.log(`${process.env.DB_NAME} database connected successfully `);
+        }
+
+
+    });
+    await pool.end()
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import express, { type Express, type Request, type Response } from 'express';
+import { checkDBConnection } from "./config/database.ts"
 
+// Step1 - check for database connection
+await checkDBConnection();
+
+// Step2 - create a server
 const app: Express = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
 app.get('/', (req: Request, resp: Response) => {
     resp.send('Hello World!');
