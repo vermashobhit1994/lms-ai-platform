b309e9148a7b2bf9911e6d56154ed45cc6f1d8d6
vermashobhit1994 <vermashobhit1994@gmail.com>
2026-07-16 08:42:39 +0530

    feat: auth backend implementation: create single script to create empty database and rename it to reflect empty database

---
+++ backend/src/db/scripts/01_create_empty_database.sql
@@ -0,0 +1,16 @@
+-- WARNING: This script will DESTROY ALL DATA in the database
+-- Creates the app database
+
+-- cmd or powershell command to run this script
+--   psql -U postgres -d postgres -v ON_ERROR_STOP=1 -f scripts/01_create_database.sql
+
+-- psql shell command to run this script
+--  \i J:/projects/internmo/lms-ai-platform/backend/src/db/scripts/01_create_database.sql
+
+SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE datname = 'lms_ai_db'
+  AND pid <> pg_backend_pid();
+
+DROP DATABASE IF EXISTS lms_ai_db;
+CREATE DATABASE lms_ai_db;
