90fe0bf106e84cdd720abcfb4b7296483be6e23e
vermashobhit1994 <vermashobhit1994@gmail.com>
2026-07-18 20:03:17 +0530

    feat: auth frontend implementation: create user registration form and send data via api endpoint to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,88 @@ import './App.css'
 function App() {
 
 
-  return <h1>Hello world</h1>
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const full_name = formData.get('full_name')
+    const email = formData.get('email')
+    const password = formData.get('password')
+    const role = formData.get("role");
+
+    console.log({ full_name, email, password, role })
+
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/v1/auth/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          full_name,
+          email,
+          password,
+          role
+        }),
+      })
+      const data = await res.json()
+      if (!res.ok) {
+        throw new Error(`Login failed ${data}`);
+      }
+      console.log('Login successful:', data)
+    } catch (error) {
+      if (error instanceof TypeError) {
+        console.log(error.message)
+      } else if (error instanceof Error) {
+        console.log(error.message)
+      } else {
+        console.log("something went wrong")
+      }
+
+    }
+  }
+  return (
+
+    <form onSubmit={handleSubmit}>
+      <label htmlFor=""> Enter full name &nbsp;
+        <input name="full_name" type="text" placeholder="Name" required />
+      </label>
+      <br />
+      <br />
+
+      <label htmlFor=""> Enter email Id &nbsp;
+        <input name="email" type="email" placeholder="Email" required />
+      </label>
+      <br />
+      <br />
+
+      <label htmlFor=""> Enter password &nbsp;
+        <input name="password" type="password" placeholder="Password" required />
+      </label>
+
+
+      <br />
+      <br />
+
+      <label htmlFor="">select your role &nbsp;&nbsp;
+        <input type="radio" name="role" id="" value="admin" required />
+        <label htmlFor="">Admin</label>
+        &nbsp;&nbsp;
+
+        <input type="radio" name="role" id="" value="instructor" required />
+        <label htmlFor="">Instructor</label>
+        &nbsp;&nbsp;
+
+        <input type="radio" name="role" id="" value="student" required />
+        <label htmlFor="">Student</label>
+
+      </label>
+      <br />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
 
 export default App
