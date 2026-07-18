c9b402574f645920b809a777d27653307f83af4f
vermashobhit1994 <vermashobhit1994@gmail.com>
2026-07-18 20:07:16 +0530

    feat: auth backend implementation: enable cors to accept POST request from frontend for specific route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express, { type Express, type Request, type Response, type NextFunction }
 import { checkDBConnection } from "./config/database.ts"
 
 import { registerUser } from "./midddleware/authenticate.ts";
-
+import cors from "cors";
 
 // Step1 - check for database connection
 await checkDBConnection();
@@ -11,15 +11,23 @@ await checkDBConnection();
 const app: Express = express();
 const port = process.env.SERVER_PORT;
 
-// Step3 - register JSON body parser before routes/middleware
+// Step3 - enable cors for frontend to enable origins
+let corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+}
+
+// Step4 - register JSON body parser before routes/middleware
 app.use(express.json());
 
-app.use("/api/v1/auth/register", (req: Request, res: Response, next: NextFunction) => {
+app.use("/api/v1/auth/register", cors(corsOptions), (req: Request, res: Response, next: NextFunction) => {
     console.log(req.body);
 
     registerUser(req, res);
     next();
 });
+
+
 
 app.listen(port, () => {
     console.log(`Backend Express server listening on port ${port}`);
